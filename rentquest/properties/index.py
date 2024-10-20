7f3486bf334f6f571c80ba81693a13c3baee49b9
roshankhanxox <64058135+roshankhanxox@users.noreply.github.com>
2024-10-20 08:43:19 +0530

    Delete rentquest directory

--- rentquest/properties/index.py
+++
@@ -1,6 +0,0 @@
-import algoliasearch_django as algoliasearch
-from .models import Property, PropertyImage  # Import your Property model
-
-# Register the Property model with Algolia
-algoliasearch.register(Property)
-algoliasearch.register(PropertyImage)
